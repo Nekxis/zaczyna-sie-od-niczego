332424ecd6762d83f5aa0762d2aa981fc7a62db2
LatekVon <latkaignacy@gmail.com>
2021-12-11 18:12:22 +0100

    added all

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 function unfold() {
+    console.log('a0');
     document.getElementById('sled').style.top = '10vh';
-
+}
+function refold() {
+    console.log('a1');
+    document.getElementById('sled').style.top = '100vh';
+}
+function unfold1() {
+    console.log('b0');
+    document.getElementById('sled0').style.left = '0';
+}
+function refold1() {
+    console.log('b1');
+    document.getElementById('sled0').style.left = '100vw';
 }
